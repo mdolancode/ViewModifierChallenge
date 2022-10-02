36d2f79655acab78d8722c2f201cb29b63f228ce
Matthew Dolan <matthew.dolan@ballysinteractive.com>
2022-10-02 07:33:39 -0700

    Initial Commit

---
+++ ViewModifierChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewModifierChallenge
+//
+//  Created by Matthew Dolan on 2022-10-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ViewModifierChallenge/ViewModifierChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModifierChallengeApp.swift
+//  ViewModifierChallenge
+//
+//  Created by Matthew Dolan on 2022-10-02.
+//
+
+import SwiftUI
+
+@main
+struct ViewModifierChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
